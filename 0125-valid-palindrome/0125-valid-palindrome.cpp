2e7873ee5d92bb247442c9498b7a2a26386894e2
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2023-06-27 14:24:32 +0530

    Time: 5 ms (67.46%), Space: 7.3 MB (92.56%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        int end = s.size()-1;
+        int start = 0;
         
-        int n = s.size();
-        string result = "";
-        
-        for (char c : s) {
-            if (isalnum(c)) {
-                result += tolower(c);
-            }
-        }
-        
-        int start = 0, end = result.size() - 1;
         while(start < end) {
-            if(result[start++] != result[end--])
+            if(!isalnum(s[start]) || s[start] == ' ')
+            {
+                // cout << "not alnum s++" << endl;
+                start++;
+                continue;
+            }
+            if(!isalnum(s[end]) || s[end] == ' '){
+                // cout << "not alnum e--" << endl;
+                end--;
+                continue;
+            }
+            
+            char a = tolower(s[start]);
+            // cout << " a is " << a << endl;
+            char b = tolower(s[end]);
+            // cout << " b is " << b << endl;
+            
+            if(a != b){
+                // cout << "a != b" <<endl;
                 return false;
+            }
+            
+            start++;
+            end--;
         }
         
         return true;
