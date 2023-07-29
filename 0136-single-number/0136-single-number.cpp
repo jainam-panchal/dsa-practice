1deb86c8391902799ae358d3509664ed3106ab99
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2023-07-29 23:10:46 +0530

    Time: 21 ms (60.23%), Space: 20.2 MB (8.19%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
+        // edge case
         if(nums.size() == 1) {
             return nums[0];
         }
             
+        // unordered map to store all occurence
         unordered_map<int, int> map;
-        
         for(int i=0 ; i<nums.size() ; i++){
             map[nums[i]]++;
         }
         
+        // finding element with 1 occurance
         for(auto i : map) {
             if(i.second == 1) {
                 return i.first;
             }
         }
         
-        return nums[0];
+        // doesn't matter what it is
+        return -1;
     }
 };
