71c85f92a78a3bbc46c14b071f02a8677f688b99
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2023-12-31 18:16:16 +0530

    Time: 3 ms (47.38%), Space: 6.7 MB (41.15%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-       
+        
+        // BRUTEFORCE APPROACH - O(N^2)
+        // int ans = -1;
+        // for(int i=0 ; i<s.size() ; i++) {
+        //     for(int j=i+1 ; j<s.size() ; j++) {
+        //         if(s[i] == s[j]) 
+        //             ans = max(ans, j - i - 1);
+        //     }
+        // }
+        
+        // OPTIMAL - O(N)
+        unordered_map<char,int> hashmap;
         int ans = -1;
-        for(int i=0 ; i<s.size() ; i++) {
-            for(int j=i+1 ; j<s.size() ; j++) {
-                if(s[i] == s[j]) 
-                    ans = max(ans, j - i - 1);
+        
+        for(int i=0; i<s.length() ; i++) {
+            if(hashmap.find(s[i]) != hashmap.end()) {
+                ans = max(ans , i - hashmap[s[i]] - 1);
+            }
+            else {
+                hashmap[s[i]] = i;
             }
         }
         
