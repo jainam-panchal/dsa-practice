c870879a1a0fc0225a65b1467892f908d403e4b0
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-01-01 21:29:43 +0530

    Time: 30 ms (8.55%), Space: 22.7 MB (22.63%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,28 +3,40 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         
         // BRUTEFORCE APPROACH O(N), O(1)
+//         int n = nums.size();
+//         if (n == 1) return nums[0];
+        
+//         for(int i=0 ; i<n ; i++) {
+            
+//             if(i == 0) {
+//                 if(nums[i] != nums[i+1]) 
+//                     return nums[i];        
+//             }
+            
+//             else if(i == n-1) {
+//                 if(nums[i] != nums[i-1])
+//                         return nums[i];
+//             }
+            
+//             else {
+//                 if(nums[i] != nums[i-1] && nums[i] != nums[i+1])
+//                     return nums[i];
+//             }    
+//         }
+        
+//         return -1;
+        
+        
+        // BRUTEFORCE APPROACH O(N), O(1)
+        
         int n = nums.size();
-        if (n == 1) return nums[0];
-        
-        for(int i=0 ; i<n ; i++) {
-            
-            if(i == 0) {
-                if(nums[i] != nums[i+1]) 
-                    return nums[i];        
-            }
-            
-            else if(i == n-1) {
-                if(nums[i] != nums[i-1])
-                        return nums[i];
-            }
-            
-            else {
-                if(nums[i] != nums[i-1] && nums[i] != nums[i+1])
-                    return nums[i];
-            }    
+        int ans = 0;
+        for (int i=0 ; i<n ; i++) {
+            // just xor with everything and you will end up with the single one which hasn't been cancelled out
+            ans = ans ^ nums[i];
         }
+        return ans;
         
-        return -1;
         
     }
 };
