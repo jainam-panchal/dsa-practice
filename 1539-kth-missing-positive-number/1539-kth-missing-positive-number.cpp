806fddeedd25ac11a065ba51ffdfc831232f80b1
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-01-04 23:21:20 +0530

    Time: 3 ms (79.19%), Space: 10 MB (63.62%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,29 +1,53 @@
 class Solution {
+
 public:
     int findKthPositive(vector<int>& nums, int k) {
-        int num = 1;
         
-        int i=0;
-        int n=nums.size();
+        // Binary Search : O(logn)
+        // Strivers ftw
         
-        while(i < n && k > 0) {
-            // not missing
-            if(nums[i] == num) {
-                i++;
-            }
-            // missing
-            else {
-                k--;
-            }
-            num++;
+        int start = 0;
+        int end = nums.size()-1;
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            int missingNumbers = nums[mid] - (mid + 1);
+            if(missingNumbers < k)
+                start = mid + 1; 
+            else
+                end = mid - 1;
+
         }
         
-        // edge case
-        // {1,2,3}, k=2
-        while(k--) {
-            num++;
-        }
-        
-        return num-1;
+        // return k + end + 1; 
+        return start + k;
     }
 };
+
+
+//         // BRUTEFORCE: O(N)
+//         // https://www.youtube.com/watch?v=BltzCxN1vRc
+        
+//         int num = 1;
+//         int i=0;
+//         int n=nums.size();
+        
+//         while(i < n && k > 0) {
+//             // not missing
+//             if(nums[i] == num) {
+//                 i++;
+//             }
+//             // missing
+//             else {
+//                 k--;
+//             }
+//             num++;
+//         }
+        
+//         // edge case
+//         // {1,2,3}, k=2
+//         while(k--) {
+//             num++;
+//         }
+        
+//         return num-1;
