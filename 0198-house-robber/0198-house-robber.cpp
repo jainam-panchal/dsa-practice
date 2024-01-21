0d76293502fd44f01da65238c240edd2cdaa315f
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-01-21 13:37:55 +0530

    Time: 4 ms (25.45%), Space: 9.2 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,27 +1,53 @@
 class Solution {
-    int Recursion(int i, vector<int> &nums, vector<int> &dp)
-    {
-        
-        if (i == 0)
-            return nums[i];
-        if (i < 0)
-            return 0;
-        
-        if(dp[i] != -1)
-            return dp[i];
-        
-        int pick = nums[i] + Recursion(i - 2, nums, dp);
-        int nonPick = 0 + Recursion(i - 1, nums, dp);
-        
-        dp[i] = max(pick, nonPick);
-        
-        return dp[i];
-    }
 public:
+    
+    // TABULAR
+    
     int rob(vector<int>& nums) {
-        int ans;
-        vector<int> dp (nums.size(), -1);
-        ans = Recursion(nums.size()-1, nums, dp);
-        return ans;
+        //tb[i] means max steal of i houses
+        vector<int> tb(nums.size()+1);
+        
+        // no house max profit
+        tb[0] = 0;
+        
+        // one house max profit
+        tb[1] = nums[0];
+        
+        for(int i=2 ; i<= nums.size() ; i++) {
+            int steal = nums[i-1] + tb[i-2];
+            int skip = 0 + tb[i-1];
+            
+            tb[i] = max(steal , skip);
+        }
+        
+        return tb[nums.size()];
     }
 };
+
+
+// MEMOIZATION
+
+// class Solution {
+// public:
+//     
+//     int t[101];
+//     int solve(vector<int>& nums, int i , int n) {
+//         if(i >= n)
+//             return 0;
+        
+//         if(t[i] != -1)
+//             return t[i];
+        
+//         int steal = nums[i] + solve(nums, i+2, n);
+//         int skip = 0 + solve(nums, i+1, n);
+        
+//         return t[i] = max(steal , skip);
+//     }
+    
+//     int rob(vector<int>& nums) {
+//         memset(t, -1, sizeof(t));
+//         int n = nums.size();
+        
+//         return solve(nums, 0 ,n);
+//     }
+// };
