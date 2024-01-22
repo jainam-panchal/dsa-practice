36df8c0a6fda305ddf110a51b9260fe14f7805a5
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-01-22 22:41:31 +0530

    Time: 14 ms (98.07%), Space: 23.6 MB (37.21%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {        
+        int N = nums.size();
         
-        // using in-place array as hashmap
-        // mapping 1 to n -> 0 to n-1
+        long long x = 0;  // duplicate - missing 
+        long long y = 0;  // duplicate^2 - missing^2
         
-        int dup = 0;
-        
-        for(int i=0 ; i<nums.size() ; i++) {
-            int index = abs(nums[i]) - 1;
-            
-            if(nums[index] < 0) {
-                dup = index + 1;
-            }
-            
-            nums[index] = - nums[index];
+        for(int i=1 ; i<=N ; i++) {
+            x += nums[i-1] - i;
+            y += pow(nums[i - 1], 2) - pow(i, 2);
         }
         
-        int miss = 0;
-        for(int i=0 ; i<nums.size() ; i++) {
-            if(nums[i] > 0 && (i+1) != dup) {
-                miss = i+1;
-                return {dup , miss};
-            }       
-        }
-        
-        return {dup , miss};
+        int missing = (y - pow(x,2)) / (2 * x);
+        int duplicate = missing + x;
+    
+        return {duplicate, missing};
     }
 };
