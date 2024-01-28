7bd16c2d02c37bf0947109749c140f114cb3867f
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-01-29 00:59:39 +0530

    Time: 394 ms (96.07%), Space: 154.2 MB (59.71%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -2,45 +2,38 @@ class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& m, int target) {
         
-        
-        // prefix sum method
         int result = 0;
         int rows = m.size();
         int cols = m[0].size();
         
-        // calc each row commulative sum
-        // ex row : 1 2 3 to 1 3 6
-        for(int row=0 ; row<rows ; row++) {
-            for(int col=1 ; col<cols ; col++) {
-                m[row][col] = m[row][col] + m[row][col-1];
+        for(int row = 0 ; row < rows ; row++) {
+            for(int col = 1 ; col < cols ; col++) {
+                m[row][col] += m[row][col-1];
             }
         }
         
-        // just like Leetcode - 560, but while iterating through each ele in row horizontally
-        // iterate through the column 
-        for(int startingCol = 0 ; startingCol < cols ; startingCol++) {
+        // pick the corner
+        for(int startCol = 0 ; startCol < cols ; startCol++) {
             
-            for(int curCol = startingCol ; curCol < cols ; curCol++) {
+            // from the corner, iterate through all cols
+            for(int currCol = startCol ; currCol < cols ; currCol++) {
                 
-                // now go through each elem in col
+                unordered_map<int,int> map;
+                map[0] = 1;         // for the edge case
                 int sum = 0;
-                unordered_map<int,int> mp;
-                mp[0] = 1;
                 
-                for(int row= 0; row<rows ; row++) {
-                    // we consider all left side of the element in the commulative sum so
-                    // we need to subtract the prev element to get the current element
-                    sum += m[row][curCol] - (startingCol > 0 ? m[row][startingCol-1] : 0);
-                    if(mp.count(sum - target)) {
-                        result += mp[sum - target];
+                for(int row = 0 ; row < rows ; row++) {
+                    
+                    // add the cummulative sum and remove the sum before startCol
+                    sum += m[row][currCol] - (startCol > 0 ? m[row][startCol-1] : 0);
+                    if(map.count(sum - target)) {
+                        result += map[sum - target];
                     }
                     
-                    mp[sum]++;
-                    
+                    map[sum]++;
                 }
                 
             }
-            
         }
         
         return result;
