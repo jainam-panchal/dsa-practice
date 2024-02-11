e737a04bd83b359bf7c15ff589ee9351d5aa4e36
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-02-11 11:55:23 +0530

    Time: 24 ms (27.57%), Space: 21.6 MB (12.89%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-    bool checkPalindrome(string &s, int i, int j) {
+    bool checkPalindrome(string &s, int i, int j, vector<vector<int>> &dp) {
         if(i > j) return true;
         
-        if(s[i] == s[j])
-            return checkPalindrome(s, i+1, j-1);
+        if(dp[i][j] != -1) {
+            return dp[i][j] == 1 ? true : false;
+        }
         
-        return false;
+        bool flag = false;
+        
+        if(s[i] == s[j]) {
+            flag = flag | checkPalindrome(s, i+1, j-1, dp);
+        }
+        
+        dp[i][j] = flag ? 1 : 0;
+        return  flag;
     }
     
     int countSubstrings(string s) {
@@ -17,10 +25,11 @@ public:
         
         int count = 0;
         int n = s.size();
+        vector<vector<int>> dp(n , vector<int>(n,-1));
         
         for(int i=0 ; i<n ; i++) {
             for(int j=i ; j<n ; j++) {
-                if(checkPalindrome(s,i,j))
+                if(checkPalindrome(s,i,j,dp))
                     count++;
             }
         }
