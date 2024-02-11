c19b24b6820f6c5cad35a57e3dbb329e59537dcd
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-02-11 15:35:56 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (7.05%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,28 +11,50 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // Approach 2 -> get the difference and move along
+        ListNode* fastptr = head;
+        ListNode* slowptr = head;
         
-        // Approach 1 -> get count - n and remove the element
-        int count = 0;
-        ListNode* temp = head;
-        while(temp != NULL) {
-            temp = temp->next;
-            count++;
-        }
-        n = count - n;
-        
-        // edge case: remove the first node
-        if(n == 0) {
-            return head->next;
+        for(int i=0 ; i<n ; i++) {
+            fastptr = fastptr->next;
         }
         
-        // remove the node
-        temp = head;
-        for(int i = 0; i < n-1; i++) {
-            temp = temp->next;
+        // EDGE CASE
+        if(fastptr == NULL) {
+            ListNode* temp = head->next;
+            delete head;
+            return temp;
         }
         
-        temp->next = temp->next->next;
+        while(fastptr->next != NULL) {
+            fastptr = fastptr->next;
+            slowptr = slowptr->next;
+        }
+        
+        slowptr->next = slowptr->next->next;
         return head;
     }
 };
+
+//         // Approach 1 -> get count - n and remove the element
+//         int count = 0;
+//         ListNode* temp = head;
+//         while(temp != NULL) {
+//             temp = temp->next;
+//             count++;
+//         }
+//         n = count - n;
+        
+//         // edge case: remove the first node
+//         if(n == 0) {
+//             return head->next;
+//         }
+        
+//         // remove the node
+//         temp = head;
+//         for(int i = 0; i < n-1; i++) {
+//             temp = temp->next;
+//         }
+        
+//         temp->next = temp->next->next;
+//         return head;
