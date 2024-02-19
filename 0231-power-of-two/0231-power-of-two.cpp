dacff60aa8c7d7a350cb095f9986b53e582d6b98
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-02-19 11:52:20 +0530

    Time: 2 ms (39.05%), Space: 7.1 MB (39.77%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -6,10 +6,15 @@ public:
             return true;
         }
         
-        if(n <= 0 || n % 2 != 0) {
+        if(n <= 0) {
             return false;
         }
         
-        return isPowerOfTwo(n/2);
+        // if the number is power of binary then it will only have one set bit
+        // 4 = 100 , 3 = 11
+        // 16 = 10000, 15 = 1111
+        // if we do and then the ans would be 0
+        
+        return (n & (n-1)) == 0;
     }
 };
