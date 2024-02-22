31928dd68e2693f2c725a7a9d83c77fb126a6f5d
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-02-22 17:34:14 +0530

    Time: 4 ms (32.04%), Space: 8.1 MB (12.51%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,28 +1,28 @@
 class Solution {
+private:
+    int nCr(int n, int r) {
+        long long ans = 1;
+        
+        for(int i=0 ; i<r ; i++) {
+            ans = ans * (n-i);
+            ans = ans / (i+1);
+        }
+        
+        return (int) ans;
+    }
+    
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        ans.push_back({1});
-
         
-        if(numRows == 1) {
-            return ans;
-        }
-        
-        for(int i=1 ; i<numRows ; i++) {
-
-            vector<int> curr(i+1, 0);
-            ans.push_back(curr);
-            
-            for(int j=0 ; j<=i ; j++) {
-                int left = (j - 1 >= 0) ? ans[i - 1][j - 1] : 0;
-                int right = (j < i) ? ans[i - 1][j] : 0;
-                
-                ans[i][j] = (left+right);
+        for(int row=1 ; row <= numRows ; row++) {
+            vector<int> temp;
+            for(int col=1 ; col <= row ; col++) {
+                temp.push_back(nCr(row - 1, col - 1));
             }
+            ans.push_back(temp);
         }
         
         return ans;
-        
     }
 };
