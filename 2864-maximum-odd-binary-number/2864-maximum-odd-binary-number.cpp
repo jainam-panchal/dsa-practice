0e88601d64c1a5500942dc0c05bad20f7c00919b
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-03-01 21:43:46 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (23.97%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -3,23 +3,11 @@ public:
     string maximumOddBinaryNumber(string s) {
         int countOnes = 0;
         
-        
         for(int i=0 ; i<s.size() ; i++) {
             if(s[i] == '1') countOnes++;
         }
-        
         int countZeros = s.size()-countOnes;
         
-        string ans = "";
-        // put leading ones and save last one for the last
-        for(int i=0 ; i<countOnes-1 ; i++) {
-            ans += '1';
-        }
-        
-        for(int i=0 ; i<countZeros ; i++) {
-            ans += '0';
-        }
-        
-        return ans + '1';
+        return string(countOnes-1, '1') + string(countZeros,'0') + '1';
     }
 };
