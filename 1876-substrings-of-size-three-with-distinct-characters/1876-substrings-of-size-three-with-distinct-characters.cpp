ff6b09e1d6e22ca498c924e04e7931d57c005932
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-04-01 23:31:28 +0530

    Time: 3 ms (60.24%), Space: 7.5 MB (57.71%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    bool checkAllUnique(vector<int> &arr) {
+        for(auto val : arr) {
+            if(val > 1) return false;
+        }
+        
+        return true;
+    }
+    
+public:
+    int countGoodSubstrings(string s) {
+        
+        int n = s.size(), k = 3;
+        vector<int> map(26, 0);
+        int result = 0 , distinctEle = 0;
+        
+        int i=0, j=0;
+        
+        while(j < n) {
+            if(map[s[j] - 'a'] == 0) {
+                distinctEle++;
+            }
+            map[s[j] - 'a']++;
+            
+            if(j-i+1 < k) {
+                j++;
+            }
+            else if(j-i+1 == k) {
+                if(distinctEle == 3) result++;
+                
+                map[s[i] - 'a']--;
+                if(map[s[i] - 'a'] == 0) distinctEle--;
+                
+                i++;
+                j++;
+            }        
+        }
+        
+        return result;
+    }
+};
