bead5e0db6f10d16fc854115ad84bb13a9c5b529
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-05-29 21:57:22 +0530

    Time: 3 ms (51.92%), Space: 7.5 MB (76.47%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,42 +1,22 @@
 class Solution {
-private:
-    void addone(string &s) {
-        
-        // Because when we add one to a binary
-        // one will be only added to the first 0 from right to left
-        
-        int i = s.size() - 1;
-        while(i >= 0 && s[i] == '1') {
-            s[i] = '0';
-            i--;
-        }
-        if(i < 0) {
-            s = '1' + s;
-        } else {
-            s[i] = '1';
-        }
-        
-    }
-    
 public:
     int numSteps(string s) {
+        int carry = 0;
+        int steps = 0;
         
-        int count = 0;
-        
-        while(s != "1") {
-            
-            int k = s.size()-1;
-            // odd case
-            if(s[k] == '1') {
-                addone(s);
+        int n = s.size() - 1;
+        while(n > 0) {
+            if(((s[n] - '0') + carry) % 2 == 1) {
+                steps += 2;
+                carry = 1;
             } 
-            // even case
             else {
-                s.pop_back();
+                steps++;
             }
             
-            count++;
+            n--;
         }
-        return count;
+        
+        return steps + carry;
     }
 };
