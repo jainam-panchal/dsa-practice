6b98ff948585c6af19b6ba8420b86f11d6ed5b5a
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-08-22 18:10:18 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (75.25%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -3,10 +3,9 @@ public:
     int findComplement(int num) {
         int no_of_bits = (int)(log2(num)) + 1;
         
-        for(int i=0 ; i<no_of_bits ; i++) {
-            num = num ^ (1 << i);
-        }
+        // example : 1 << 3 => 1000 - 1 => 111
+        unsigned int mask = (1U << no_of_bits) - 1;
         
-        return num;
+        return num ^ mask;
     }
 };
