e64bc994f8bf70c85aef8bc38879cba8a2c18a14
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-08-28 22:03:55 +0530

    Time: 221 ms (52.97%), Space: 96.8 MB (54.47%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -0,0 +1,49 @@
+class Solution {
+private:
+    bool check(vector<vector<int>>& grid1, vector<vector<int>>& grid2 , int i , int j) {
+        
+        // out of bound
+        if(i < 0 || i >= grid1.size() || j < 0 || j >= grid1[0].size()) {
+            return true;
+        }
+       
+        // we only care about land
+        if(grid2[i][j] != 1) {
+            return true; // just finish exploring
+        }
+        
+        // mark visited
+        grid2[i][j] = -1;
+        
+        // grid1 must have one
+        bool result = (grid1[i][j] == 1);
+        
+        result = result & check(grid1 , grid2 , i + 1 , j);
+        result = result & check(grid1 , grid2 , i - 1 , j);
+        result = result & check(grid1 , grid2 , i , j + 1);
+        result = result & check(grid1 , grid2 , i , j - 1);
+        
+        return result;
+    }
+    
+public:
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        
+        int count = 0;
+        int n = grid2.size() , m = grid2[0].size();
+        
+        for(int i=0 ; i<n ; i++) {
+            for(int j=0 ; j<m ; j++) {
+                
+                // found an island
+                if(grid2[i][j] == 1 && check(grid1 , grid2 , i , j)) {
+                    count++;
+                }
+                
+            }
+        }
+        
+        
+        return count;
+    }
+};
