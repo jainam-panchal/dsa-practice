e6cea10d5ee509c3d160585ff15f1a7adaf18a72
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2024-09-28 10:31:10 +0530

    Time: 288 ms (25.68%), Space: 16.5 MB (63.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,17 +1,24 @@
 class Solution {
 
 private:
-    bool helper(vector<int> &nums , int n , int sum, vector<vector<int>> &dp) {
-        if(sum == 0) return true;
+    bool helper(vector<int> &nums , int n , int sum, vector<vector<bool>> &dp) {
+        for(int i=0 ; i <n+1 ; i++)
+            dp[i][0] = true;
         
-        if(n <= 0) return false;
+        for(int i=1 ; i<sum+1 ; i++)
+            dp[0][i] = false;
         
-        if(dp[n][sum] != -1) return dp[n][sum];
+        for(int i=1 ; i<n+1 ; i++) {
+            for(int j=1 ; j<sum+1 ; j++) {
+                if(nums[i - 1] > j) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j - nums[i-1]];
+                }
+            }
+        }
         
-        if(nums[n-1] > sum)
-            return dp[n][sum] = helper(nums , n-1 , sum, dp);
-        
-        return dp[n][sum] = helper(nums , n-1 , sum, dp) || helper(nums , n-1 , sum - nums[n - 1], dp);
+        return dp[n][sum];
     }    
     
 public:
@@ -24,7 +31,7 @@ public:
         
         sum /= 2;
         
-        vector<vector<int>> dp(n + 1 , vector<int>(sum + 1, -1));
+        vector<vector<bool>> dp(n + 1 , vector<bool>(sum + 1));
         return helper(nums , n , sum , dp);
         
     }
